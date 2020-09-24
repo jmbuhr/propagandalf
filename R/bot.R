8c649fd9ff88f622b3b94019e9b1a30a29d7d669
Jannik Buhr <jannik.m.buhr@gmail.com>
2020-09-24 11:22:12 +0200

    fix bot

--- R/bot.R
+++ R/bot.R
@@ -3,8 +3,10 @@ library(telegram.bot)
 library(meme)
 
 token <- bot_token("propagandalf")
-# bot <- Bot(token)
+bot <- Bot(token)
 updater <- Updater(token)
+
+
 
 start <- function(bot, update){
   bot$sendMessage(chat_id = update$message$chat_id,
@@ -13,39 +15,52 @@ start <- function(bot, update){
 
 gandalf <- function(bot, update, args) {
   chat_id <- update$message$chat_id
-  if (length(args > 0L)) {
+  # message(chat_id)
+  if (length(args >= 1L)) {
     text <- paste(args, collapse = " ")
     text <- stringr::str_split(text, ";")[[1]]
     upper <- text[1]
-    lower <- text[2:length(text)]
-    cat(upper)
-    cat(lower)
-
+    if (length(text) >= 2) {
+      lower <- text[2:length(text)]
+    } else {
+      lower <- ""
+    }
     templates <- dir("img", full.names = TRUE)
     img_path <- sample(templates, 1)
 
     img <- magick::image_read(img_path)
     prop <- magick::image_info(img)
     tmp <- tempfile(fileext = ".png")
-    cat(tmp)
+    m <- meme(img = img_path,
+              font = "Helvetica",
+              upper = upper,
+              lower = lower)
     png(tmp,
         width = prop$width,
         height = prop$height)
-    meme(img = img_path,
-         font = "Helvetica",
-         upper = upper,
-         lower = lower)
-    invisible(dev.off())
+    print(m)
+    dev.off()
     bot$sendPhoto(chat_id = chat_id,
                   photo = tmp)
+    request <- glue::glue("https://api.telegram.org/bot{bot_token('propagandalf')}/sendPhoto?chat_id={chat_id}")
+    # httr::POST(request,
+    #            body = list(photo = httr::upload_file(tmp, type = "png")),
+    #            encode = "multipart")
   } else {
     bot$sendMessage(chat_id = chat_id,
                     text = "Is there something you want me to meme? Pass is right after the command (and you SHALL pass). Separate upper and lower text by semicolon ;")
   }
 }
 
+error_callback <- function(bot, error) {
+  warning(simpleWarning(conditionMessage(error), call = "Updates polling"))
+}
+
 updater <- updater +
+  ErrorHandler(error_callback) +
   CommandHandler("start", start) +
   CommandHandler("gandalf", gandalf, pass_args = TRUE)
 
 updater$start_polling()
+
+
